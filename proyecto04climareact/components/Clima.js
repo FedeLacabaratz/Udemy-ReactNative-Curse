a288a8175a11f8757a286d8389779fc4265408be
Fede Lacabaratz <lacarq@gmail.com>
2020-08-25 11:24:10 +0200

    add updates and keep on working

---
+++ proyecto04climareact/components/Clima.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const Clima = () => {
+    return (  
+        <>
+            <Text>Desde Clima</Text>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    clima: {
+
+    },
+});
+ 
+export default Clima;
