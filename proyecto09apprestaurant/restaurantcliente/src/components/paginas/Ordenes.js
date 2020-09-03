63db5c52cc06d9327c5971ce6b0b5779c967e427
Fede Lacabaratz <lacarq@gmail.com>
2020-09-03 12:24:20 +0200

    add updates and stop on the web client side for now, going back to the app side

--- proyecto09apprestaurant/restaurantcliente/src/components/paginas/Ordenes.js
+++ proyecto09apprestaurant/restaurantcliente/src/components/paginas/Ordenes.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { FirebaseContext } from '../../firebase';
+import Orden from '../ui/Orden';
 
 const Ordenes = () => {
+
+    // Context con las operaciones de firebase
+    const { firebase } = useContext(FirebaseContext);
+
+    // state con las ordenes
+    const [ordenes, setOrdenes] = useState([]);
+
+    useEffect(() => {
+        const obtenerOrdenes = async () => {
+            await firebase.db.collection('ordenes').where('completado', '==', false).onSnapshot(manejarSnapshot)
+        }
+        obtenerOrdenes();
+    }, []);
+
+    function manejarSnapshot(snapshot) {
+        const ordenes = snapshot.docs.map(doc => {
+            return {
+                id: doc.id,
+                ...doc.data()
+            }
+        });
+        setOrdenes(ordenes);
+    }
+
     return (
         <>
             <h1 className="text-3xl font-light mb-4">Ordenes</h1>
+            <div className="sm:flex sm:flex-wrap -mx-3">
+                {ordenes.map(orden => (
+                    <Orden
+                        key={orden.id}
+                        orden={orden}
+                    />
+                ))}
+            </div>
         </>
     );
 };
