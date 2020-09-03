67beb442d9f5c7767448816a89a53833b0ac1794
Fede Lacabaratz <lacarq@gmail.com>
2020-09-03 11:40:45 +0200

    add updates and stop on the app side for now, going back to client web side

--- proyecto09apprestaurant/restaurantapp/views/ProgresoPedido.js
+++ proyecto09apprestaurant/restaurantapp/views/ProgresoPedido.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Container, Text, H1, H3, Button } from 'native-base'; 
+import globalStyles from '../styles/global';
+import { useNavigation } from '@react-navigation/native';
+import PedidoContext from '../context/pedidos/pedidosContext';
+
 
 const ProgresoPedido = () => {
+
+    // Context
+    const { idPedido } = useContext(PedidoContext);
+
     return (  
-        <>
-            <Text>Desde ProgresoPedido</Text>
-        </>
+        <Text>{idPedido}</Text>
     );
 }
  
