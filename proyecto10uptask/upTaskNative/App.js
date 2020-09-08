e7429062deb6e1b17a9974132c38321b130bab7b
Fede Lacabaratz <lacarq@gmail.com>
2020-09-08 14:48:07 +0200

    add updates and keep on progressing on the native side

--- proyecto10uptask/upTaskNative/App.js
+++ proyecto10uptask/upTaskNative/App.js
@@ -1,5 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { Root } from 'native-base';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -11,34 +13,36 @@ import CrearCuenta from './views/CrearCuenta';
 const App = () => {
   return (
     <>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Login"
-        >
-          <Stack.Screen
-            name="Login"
-            component={Login}
-            options={{
-              title: "Iniciar Sesión",
-              headerShown: false
-            }}
-          />
-          <Stack.Screen
-            name="CrearCuenta"
-            component={CrearCuenta}
-            options={{
-              title: "Crear Cuenta",
-              headerStyle: {
-                backgroundColor: '#28303b',
-              },
-              headerTintColor: '#fff',
-              headerTitleStyle: {
-                fontWeight: 'bold'
-              }
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Root>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Login"
+          >
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{
+                title: "Iniciar Sesión",
+                headerShown: false
+              }}
+            />
+            <Stack.Screen
+              name="CrearCuenta"
+              component={CrearCuenta}
+              options={{
+                title: "Crear Cuenta",
+                headerStyle: {
+                  backgroundColor: '#28303b',
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                  fontWeight: 'bold'
+                }
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Root>
     </>
   );
 };
